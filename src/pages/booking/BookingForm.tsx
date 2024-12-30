99c8a062ab169384450685520046b6598e969dbf
ismailJihad29 <ismailhossenjihad234@gmail.com>
2024-12-30 20:42:31 +0600

    booking form update

--- src/pages/booking/BookingForm.tsx
+++ src/pages/booking/BookingForm.tsx
@@ -1,3 +1,4 @@
+
 import { useNavigate, useParams } from "react-router-dom";
 import {
   useAddBookingsMutation,
@@ -5,7 +6,6 @@ import {
   useGetFacilityDetailsQuery,
 } from "../../redux/api/facility/facilityApi";
 import {
-  Button,
   DatePicker,
   DatePickerProps,
   TimePicker,
@@ -13,12 +13,15 @@ import {
 } from "antd";
 import { TFacility } from "../../types/user";
 import dayjs, { Dayjs } from "dayjs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "sonner";
 import Loading from "../shared/Loading";
 import { useAppSelector } from "../../redux/hooks";
 import { selectCurrentUser } from "../../redux/features/userSlice";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { FaCalendarAlt, FaClock } from "react-icons/fa"; // React Icons
+import AOS from "aos"; // AOS import
+import "aos/dist/aos.css"; // AOS CSS
 
 type TOptions = {
   id?: string;
@@ -33,7 +36,7 @@ type TimeOptions = {
 const BookingForm = () => {
   const { user } = useAppSelector(selectCurrentUser);
   const { id } = useParams();
-  const { data, isError } = useGetFacilityDetailsQuery(id);
+  const { data, isError, isLoading } = useGetFacilityDetailsQuery(id);
   const facility: TFacility = data?.data?.[0];
   const [options, setOptions] = useState<TOptions | null>(null);
   const [startTimeValue, setStartTimeValue] = useState<Dayjs | null>(null);
@@ -42,11 +45,15 @@ const BookingForm = () => {
   const [isCheckAvailability, setIsCheckAvailability] = useState(false);
   const [dates, setDates] = useState<Dayjs | null>(null);
   const navigate = useNavigate();
-  const { data: checkAvailabilityData, isFetching } =
+  const { data: checkAvailabilityData,  } =
     useCheckFacilityAvailabilityQuery(options, {
       skip: !isCheckAvailability,
     });
   const [addBookings] = useAddBookingsMutation();
+
+  useEffect(() => {
+    AOS.init(); // Initialize AOS
+  }, []);
 
   const onChange: DatePickerProps["onChange"] = (date) => {
     if (date) {
@@ -135,7 +142,7 @@ const BookingForm = () => {
     }
   };
 
-  if (isFetching) {
+  if (isLoading) {
     return <Loading />;
   }
 
@@ -144,7 +151,7 @@ const BookingForm = () => {
   }
 
   return (
-    <div className="relative min-h-screen bg-gray-100 dark:bg-gray-800">
+    <div className="relative min-h-screen bg-white dark:bg-gray-900">
       {/* Hero Section */}
       <div
         className="w-full h-80 bg-cover bg-center relative"
@@ -152,83 +159,104 @@ const BookingForm = () => {
           backgroundImage: `url(${facility?.image})`,
         }}
       >
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center">
-          <h1 className="text-4xl text-white font-bold">{facility?.name}</h1>
-          <p className="text-2xl text-white mt-2">
-            ${facility?.pricePerHour} per hour
-          </p>
-        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center"></div>
       </div>
 
       {/* Booking Form Section */}
       <div className="container mx-auto px-4 lg:px-8 py-12 relative">
-        <div className="bg-white dark:bg-gray-900 rounded-lg shadow-xl p-8 mx-auto max-w-4xl -mt-28">
-          <h2 className="text-2xl font-bold text-center text-gray-800 dark:text-gray-100 mb-8">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-xl p-8 mx-auto w-[80%] -mt-28">
+          <h2 className="text-title text-center mb-8">
             Book Your Slot
           </h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {/* Date Picker */}
-            <div>
-              <h3 className="text-lg font-semibold text-gray-700 dark:text-gray-300 mb-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-14">
+            {/* Facility Image Section */}
+            <div className="relative" data-aos="fade-right">
+              <div
+                className="absolute inset-0 bg-gradient-to-br from-blue-300 to-green-400 rounded-3xl blur-xl opacity-40"
+              ></div>
+              <div className="relative z-10">
+                <img
+                  className="w-3/4 mx-auto rounded-xl shadow-lg"
+                  src={facility?.image}
+                  alt={facility?.name}
+                />
+              </div>
+            </div>
+
+            {/* Booking Form Section */}
+            <div data-aos="fade-left">
+              <div>
+                <h3 className="text-2xl font-bold mt-4">{facility?.name}</h3>
+                <p className="text-yellow-500 text-lg font-semibold">
+                  ${facility?.pricePerHour} / Hour
+                </p>
+                <p className="text-gray-600 mt-2 ">{facility?.description}</p>
+              </div>
+              <h3 className="text-lg font-semibold font-title text-hero mb-2">
                 Pick Your Date
               </h3>
-              <DatePicker
-                value={dates}
-                onChange={onChange}
-                className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200"
-              />
-              <Button
-                type="primary"
-                className="w-full mt-4 py-3 bg-indigo-600 hover:bg-indigo-700 rounded-lg"
+              <div className="flex items-center">
+                <FaCalendarAlt className="mr-2 text-xl text-blue-500" />
+                <DatePicker
+                  value={dates}
+                  onChange={onChange}
+                  className="w-full p-3 border border-gray-300 rounded-lg dark:bg-white dark:border-gray-600 dark:text-white"
+                />
+              </div>
+              <button
+                className="button-secondary w-full mt-5"
                 onClick={handleOnChangeDate}
               >
                 Check Availability
-              </Button>
-            </div>
-
-            {/* Time Pickers */}
-            {checkAvailabilityData && (
-              <div>
-                <h3 className="text-lg font-semibold text-gray-700 dark:text-gray-300 mb-2">
-                  Select Time Slot
-                </h3>
-                <div className="grid grid-cols-1 gap-4">
-                  <div>
-                    <label className="block text-gray-600 dark:text-gray-300 mb-2">
-                      Start Time
-                    </label>
-                    <TimePicker
-                      value={startTimeValue}
-                      onChange={onChangeStartTime}
-                      format="HH"
-                      className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200"
-                    />
-                  </div>
-                  <div>
-                    <label className="block text-gray-600 dark:text-gray-300 mb-2">
-                      End Time
-                    </label>
-                    <TimePicker
-                      value={endTimeValue}
-                      onChange={onChangeEndTime}
-                      format="HH"
-                      className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200"
-                    />
-                  </div>
-                </div>
-
+              </button>
+              {checkAvailabilityData && (
                 <div>
-                  <Button
-                    type="primary"
-                    className="w-full mt-4 py-3 bg-green-600 hover:bg-green-700 rounded-lg"
-                    onClick={handleProceed}
-                  >
-                    Proceed to Payment
-                  </Button>
+                  <h3 className="text-lg font-semibold font-title text-hero mb-2 mt-2">
+                    Select Time Slot
+                  </h3>
+                  <div className="grid grid-cols-1 gap-4">
+                    <div>
+                      <label className="text-subtitle mb-2">
+                        Start Time
+                      </label>
+                      <div className="flex items-center">
+                        <FaClock className="mr-2 text-xl text-green-500" />
+                        <TimePicker
+                          value={startTimeValue}
+                          onChange={onChangeStartTime}
+                          format="HH"
+                          className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label className="text-subtitle mb-2">
+                        End Time
+                      </label>
+                      <div className="flex items-center">
+                        <FaClock className="mr-2 text-xl text-red-500" />
+                        <TimePicker
+                          value={endTimeValue}
+                          onChange={onChangeEndTime}
+                          format="HH"
+                          className="w-full p-3 border border-gray-300 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      className="w-full mt-4 button-secondary"
+                      onClick={handleProceed}
+                    >
+                      Proceed to Payment
+                    </button>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
